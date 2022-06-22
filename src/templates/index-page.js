eaf356a57a441f0bba0754fbca0215cf7d42f3e3
Skeet Booth <skeet.booth@ppq.com.au>
2022-06-23 08:42:44 +1000

    Fix error

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,76 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
-import { getImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/Layout'
-// import Features from '../components/Features'
-// import BlogRoll from '../components/BlogRoll'
-import FullWidthImage from '../components/FullWidthImage'
 
 // eslint-disable-next-line
-export const IndexPageTemplate = ({ image, title, subheading }) => {
-  const heroImage = getImage(image) || image
-
+export const IndexPageTemplate = ({ title, subheading }) => {
   return (
     <div>
-      <FullWidthImage img={heroImage} title={title} subheading={subheading} />
-      <section className='section section--gradient'>
-        <div className='container'>
-          <div className='section'>
-            <div className='columns'>
-              <div className='column is-10 is-offset-1'>
-                <div className='content'>
-                  <div className='content'>
-                    <Link
-                      className='btn'
-                      to='/admin/#/collections/mdx-docs/entries'
-                    >
-                      Go edit stuff
-                    </Link>
-                  </div>
-                  {/* <div className='columns'>
-                    <div className='column is-12'>
-                      <h3 className='has-text-weight-semibold is-size-2'>
-                        {heading}
-                      </h3>
-                      <p>{description}</p>
- 
-                    </div>
-                  </div> */}
-                  {/* <Features gridItems={intro.blurbs} /> */}
-                  {/* <div className="columns">
-                    <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/products">
-                        See all products
-                      </Link>
-                    </div>
-                  </div> */}
-                  {/* <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      Latest stories
-                    </h3>
-                    <BlogRoll />
-                    <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/blog">
-                        Read more
-                      </Link>
-                    </div>
-                  </div> */}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <h1>{title}</h1>
+      <h2>{subheading}</h2>
+      <Link className='btn' to='/admin/#/collections/mdx-docs/entries'>
+        Go edit stuff
+      </Link>
     </div>
   )
 }
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
-  heading: PropTypes.string,
   subheading: PropTypes.string,
 }
 
@@ -80,13 +28,8 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
         title={frontmatter.title}
-        heading={frontmatter.heading}
         subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
       />
     </Layout>
   )
@@ -107,18 +50,7 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            gatsbyImageData(quality: 100, layout: FULL_WIDTH)
-          }
-        }
-        heading
         subheading
-        mainpitch {
-          title
-          description
-        }
-        description
       }
     }
   }
