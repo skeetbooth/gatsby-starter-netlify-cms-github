e23c2c2a114593a452f63812226bd03780154bea
Skeet Booth <skeet.booth@ppq.com.au>
2022-06-22 16:53:19 +1000

    Add an mdx file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -49,8 +49,8 @@ const TemplateWrapper = ({ children }) => {
         />
       </Helmet>
       {/* <Navbar /> */}
-      {/* <div>{children}</div>
-      <Footer /> */}
+      <div>{children}</div>
+      {/* <Footer /> */}
     </div>
   )
 }
